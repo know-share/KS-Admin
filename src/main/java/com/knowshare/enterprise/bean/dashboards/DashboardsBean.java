698e0b5adbff9bf0223de359ee10aa86b7e71013
JFBF <felichiki19@hotmail.com>
2017-09-03 16:57:11 -0500

    Estadísticas completas

--- src/main/java/com/knowshare/enterprise/bean/dashboards/DashboardsBean.java
+++ src/main/java/com/knowshare/enterprise/bean/dashboards/DashboardsBean.java
@@ -1,13 +1,18 @@
 package com.knowshare.enterprise.bean.dashboards;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.knowshare.enterprise.repository.idea.IdeaRepository;
 import com.knowshare.enterprise.repository.perfilusuario.UsuarioRepository;
 import com.knowshare.entities.academia.Carrera;
+import com.knowshare.entities.idea.Idea;
+import com.knowshare.entities.idea.Tag;
 import com.knowshare.entities.perfilusuario.Usuario;
 
 @Component
@@ -16,6 +21,9 @@ public class DashboardsBean implements DashboardsFacade {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	 
+	@Autowired
+	private IdeaRepository ideaRepository;
+	
 	@Override
 	public List<Integer> generoCarrera(String carrera) {
 		List<Usuario> usuarios = usuarioRepository.findAll();
@@ -38,5 +46,27 @@ public class DashboardsBean implements DashboardsFacade {
 		
 		return Final;
 	}
+
+	@Override
+	public Map<String, Integer> usoTags() {
+		Map<String,Integer> map = new HashMap<String,Integer>();
+		List<Idea> ideas = ideaRepository.findAll();
+		for(Idea i:ideas){
+			if(!i.getTags().isEmpty()){
+				for(Tag t: i.getTags()){
+					if(!map.containsKey(t.getNombre())){
+						map.put(t.getNombre(),1);
+					}
+					else{
+						int v = map.get(t.getNombre());
+						map.replace(t.getNombre(),v+1);
+					}
+				}	
+			}
+		}
+		return map;
+	}
+
+	
 
 }
--- src/main/java/com/knowshare/enterprise/bean/dashboards/DashboardsFacade.java
+++ src/main/java/com/knowshare/enterprise/bean/dashboards/DashboardsFacade.java
@@ -4,6 +4,9 @@
 package com.knowshare.enterprise.bean.dashboards;
 
 import java.util.List;
+import java.util.Map;
+
+
 
 /**
  * @author Pipe
@@ -12,4 +15,5 @@ import java.util.List;
 public interface DashboardsFacade {
 
 	List<Integer> generoCarrera(String carrera);
+	Map<String,Integer> usoTags();
 }
